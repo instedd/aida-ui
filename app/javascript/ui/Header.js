237fd516dff09ab1e20070ab8b7a0445ea25e148
Brian J. Cardiff <bcardiff@gmail.com>
2017-11-17 02:01:40 -0300

    Go to root

--- app/javascript/ui/Header.js
+++ app/javascript/ui/Header.js
@@ -5,7 +5,7 @@ import * as Md from 'react-md';
 
 const nav = <nav>
               <div className='sections'>
-                <Md.Button href='http://google.com' flat label="Bots" />
+                <Md.Button href='/' flat label="Bots" />
               </div>
               <UserMenu />
             </nav>
