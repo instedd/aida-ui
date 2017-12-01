eae4968a5b4b500f094b643f0ca5b2c287e84a76
Gustavo Giráldez <ggiraldez@manas.tech>
2017-12-01 18:20:42 -0300

    Add some spec regarding skills and manifest

--- spec/models/bot_spec.rb
+++ spec/models/bot_spec.rb
@@ -1,7 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Bot, type: :model do
+  let(:user) { User.create!(email: "user@example.com") }
+
   it "can create bot" do
-    Bot.create_prepared! User.create!(email: "user@example.org")
+    Bot.create_prepared! user
+  end
+
+  describe "manifest" do
+    let!(:bot) { Bot.create_prepared! user }
+    let!(:skill) { bot.skills.create_skill! 'keyword_responder' }
+
+    it "generates manifest" do
+      manifest = bot.manifest
+      expect(manifest).to_not be_nil
+      expect(manifest[:version]).to eq(1)
+      expect(manifest.keys).to match_array(%i(version languages front_desk skills variables channels))
+    end
+
+    it "outputs only enabled skills" do
+      expect(bot.manifest[:skills].size).to eq(1)
+      skill.update_attributes! enabled: false
+      bot.reload
+      expect(bot.manifest[:skills].size).to eq(0)
+    end
   end
 end
