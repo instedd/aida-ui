654e8e9bcd68cdf2e8179945a1dc1b9469f0ab14
Brian J. Cardiff <bcardiff@gmail.com>
2017-12-01 10:55:16 -0300

    Fix build. Make request specs have a user

--- spec/requests/deep_links_spec.rb
+++ spec/requests/deep_links_spec.rb
@@ -1,6 +1,9 @@
 require "rails_helper"
 
 RSpec.describe "deep links", type: :request do
+  let!(:user) { User.create! email: "user@example.com" }
+  before(:each) { sign_in user }
+
   def deep_link_success(path)
     get path
     expect(response).to be_success
