b6e54e01d432347b4989c5128a80610d3b4ab26f
Brian J. Cardiff <bcardiff@gmail.com>
2017-12-27 15:08:58 -0300

    Make travis build docker images
    Add slack notification

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -3,5 +3,5 @@ VersionFilePath = "#{::Rails.root.to_s}/VERSION"
 Rails.application.config.send "version_name=", if FileTest.exists?(VersionFilePath) then
   IO.read(VersionFilePath)
 else
-  "development"
+  "#{Settings.version}-development"
 end
