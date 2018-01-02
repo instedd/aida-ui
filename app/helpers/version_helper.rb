ac874385e102f3134c9bae3568699e0f72afbb11
Brian J. Cardiff <bcardiff@gmail.com>
2018-01-02 14:45:47 -0300

    Make backend version optional

--- app/helpers/version_helper.rb
+++ app/helpers/version_helper.rb
@@ -1,6 +1,6 @@
 module VersionHelper
   def self.version_name
     # TODO collapse if ui_version_name and Backend.version match
-    "ui: #{Rails.application.config.ui_version_name} - engine: #{Backend.version}"
+    "ui: #{Rails.application.config.ui_version_name} - engine: #{Backend.version rescue "none"}"
   end
 end
