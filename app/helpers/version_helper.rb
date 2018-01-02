7416b4c7362163284b0053473d68a6d0fa2c4a70
Brian J. Cardiff <bcardiff@gmail.com>
2018-01-02 14:17:51 -0300

    Refactor version_name to helper

---
+++ app/helpers/version_helper.rb
@@ -0,0 +1,6 @@
+module VersionHelper
+  def self.version_name
+    # TODO collapse if ui_version_name and Backend.version match
+    "ui: #{Rails.application.config.ui_version_name} - engine: #{Backend.version}"
+  end
+end
--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,6 +1,6 @@
 VersionFilePath = "#{::Rails.root.to_s}/VERSION"
 
-Rails.application.config.send "version_name=", if FileTest.exists?(VersionFilePath) then
+Rails.application.config.send "ui_version_name=", if FileTest.exists?(VersionFilePath) then
   IO.read(VersionFilePath)
 else
   "#{Settings.version}-development"
