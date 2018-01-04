7cf7365299c8b69677124e04746ca05705cee1e8
Brian J. Cardiff <bcardiff@gmail.com>
2018-01-04 16:50:27 -0300

    Make smtp settings configurable

--- config/initializers/mailing.rb
+++ config/initializers/mailing.rb
@@ -3,4 +3,8 @@ Rails.application.config.tap do |config|
   config.action_mailer.default_url_options = { protocol: base_url.scheme, host: base_url.host, port: base_url.port }
 
   config.action_mailer.asset_host = Settings.base_url
+
+  config.action_mailer.delivery_method = Settings.mailer.delivery_method.to_sym
+  config.action_mailer.smtp_settings = Settings.smtp_settings.to_h
 end
+
