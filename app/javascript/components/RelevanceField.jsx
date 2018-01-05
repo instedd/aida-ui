f16fda9f3cf3be1b3e844d4d7ccf7e4ebde0cb23
Brian J. Cardiff <bcardiff@gmail.com>
2018-01-05 18:59:37 -0300

    Fix typo

--- app/javascript/components/RelevanceField.jsx
+++ app/javascript/components/RelevanceField.jsx
@@ -8,7 +8,7 @@ import { blank } from '../utils/string'
 export const RelevanceField = ({value, onChange}) =>
   <Field id="relevant" className={`relevance-field ${blank(value) ? "blank" : ""}`}
     leftIcon={<FontIcon>traffic</FontIcon>}
-    placeholder="Releavance: Add conditions for skill use"
+    placeholder="Relevance: Add conditions for skill use"
     value={value || ""} onChange={onChange} />
   // since value might be null (config might not have relevant field),
   // force an empty string to avoid controlled vs uncontrolled component
