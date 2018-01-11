a8e38ee5111934cc2da939322e2767573e2e9ec0
Brian J. Cardiff <bcardiff@gmail.com>
2018-01-11 12:20:26 -0300

    Clear new lines at the end of version_name

--- config/initializers/version.rb
+++ config/initializers/version.rb
@@ -1,7 +1,7 @@
 VersionFilePath = "#{::Rails.root.to_s}/VERSION"
 
 Rails.application.config.send "ui_version_name=", if FileTest.exists?(VersionFilePath) then
-  IO.read(VersionFilePath)
+  IO.read(VersionFilePath).strip
 else
   "#{Settings.version}-development"
 end
