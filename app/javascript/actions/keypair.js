1b5021d64a0e2fa2aa699bcdb7b41ae73a194458
Franciscello <ftarulla@gmail.com>
2018-01-31 14:20:13 -0300

    Add key generation algorithm PBKDF2

--- app/javascript/actions/keypair.js
+++ app/javascript/actions/keypair.js
@@ -1,5 +1,6 @@
 import * as api from '../utils/api'
 import * as nacl from 'tweetnacl'
+import pbkdf2 from 'pbkdf2'
 
 export const FETCH = 'FETCH'
 export const FETCH_SUCCESS = 'FETCH_SUCCESS'
@@ -15,10 +16,16 @@ export const fetchSuccess = (encryptedKeyPair) => ({
 
 export const generateKeyPair = (passphrase) => (dispatch, getState) => {
   const keyPair = nacl.box.keyPair()
-  const password = nacl.hash(new Uint8Array(passphrase.split().map(c => c.charCodeAt(0) & 255)))
 
-  const nonce = password.slice(0, 24)
-  const key = password.slice(24, 56)
+  const key = pbkdf2.pbkdf2Sync(passphrase, '', 1000, 32, "sha512")
+  const nonce = new Uint8Array(24)
+
+  // double check that the generated key is valid since pbkdf2
+  // doesn't throws errors on missing algorithm
+  if (key.every((k)=>(k == 0))) {
+    throw "Missing hashing algorithm"
+  }
+
   const encryptedSecretKey = nacl.secretbox(keyPair.secretKey, nonce, key)
 
   dispatch(fetch())
