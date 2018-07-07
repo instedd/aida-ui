9ee880e61c79c1009768f4dfbc471c7234cbba58
Mariano Coca <marianoabelcoca@gmail.com>
2018-07-07 12:56:45 -0300

    Add rescue clause to front desk migration. Ensure threshold has a valid value on run.

--- db/migrate/20180706130248_add_unsubscribe_to_front_desk.rb
+++ db/migrate/20180706130248_add_unsubscribe_to_front_desk.rb
@@ -2,10 +2,19 @@ class AddUnsubscribeToFrontDesk < ActiveRecord::Migration[5.1]
   def up
     Bot.all.each do |bot|
       bot.behaviours.where(kind: "front_desk").each do |front_desk|
-        front_desk.config["unsubscribe_introduction_message"] = "Send UNSUBSCRIBE to stop receiving messages"
-        front_desk.config["unsubscribe_keywords"] = "UNSUBSCRIBE"
-        front_desk.config["unsubscribe_acknowledge_message"] = "I won't send you any further messages"
-        front_desk.save!
+        begin
+          front_desk.config["unsubscribe_introduction_message"] = "Send UNSUBSCRIBE to stop receiving messages"
+          front_desk.config["unsubscribe_keywords"] = "UNSUBSCRIBE"
+          front_desk.config["unsubscribe_acknowledge_message"] = "I won't send you any further messages"
+          if front_desk.config["threshold"] > 0.5
+            front_desk.config["threshold"] = 0.5
+          end
+          front_desk.save!
+        rescue => e
+          puts e.message
+          puts bot.id
+          puts front_desk.config
+        end
       end
     end
   end
@@ -13,8 +22,12 @@ class AddUnsubscribeToFrontDesk < ActiveRecord::Migration[5.1]
   def down
     Bot.all.each do |bot|
       bot.behaviours.where(kind: "front_desk").each do |front_desk|
-        front_desk.config.except!("unsubscribe_introduction_message", "unsubscribe_keywords", "unsubscribe_acknowledge_message")
-        front_desk.save!
+        begin
+          front_desk.config.except!("unsubscribe_introduction_message", "unsubscribe_keywords", "unsubscribe_acknowledge_message")
+          front_desk.save!
+        rescue => e
+          puts e.message
+        end
       end
     end
   end
