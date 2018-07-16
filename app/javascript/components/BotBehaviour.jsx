9d718e2dab4c63ff035faa1f25e0967771cf3560
aeatencio <aeatencio@gmail.com>
2018-07-16 17:08:39 -0300

    Fix bug bots cannot be published
    By adapting on BotBehaviour component some of the BotLayout code.
    From where publishBot method was moved in ade6206.
    
    Fixes instedd/aida#228

--- app/javascript/components/BotBehaviour.jsx
+++ app/javascript/components/BotBehaviour.jsx
@@ -8,10 +8,13 @@ import SkillsBar from './SkillsBar'
 import BotSkill from './BotSkill'
 import { hasPermission } from '../utils'
 import ContentDenied from './ContentDenied'
+import { bindActionCreators } from 'redux'
+import { withRouter } from 'react-router'
+import { connect } from 'react-redux'
 
-export class BotBehaviour extends Component {
+export class BotBehaviourComponent extends Component {
   render() {
-    const { bot, onToggleChatWindow } = this.props
+    const { bot, onToggleChatWindow, botActions } = this.props
 
     if (!hasPermission(bot, 'manages_behaviour')) {
       return <ContentDenied />
@@ -33,3 +36,22 @@ export class BotBehaviour extends Component {
     )
   }
 }
+
+const mapStateToProps = (state, {match}) => {
+  let bot = null
+
+  if (state.bots.items) {
+    bot = state.bots.items[match.params.id] || null
+  }
+
+  return {
+    botsLoaded: state.bots.items != null,
+    bot: bot
+  }
+}
+
+const mapDispatchToProps = (dispatch) => ({
+  botActions: bindActionCreators(botActions, dispatch)
+})
+
+export const BotBehaviour = withRouter(connect(mapStateToProps, mapDispatchToProps)(BotBehaviourComponent))
