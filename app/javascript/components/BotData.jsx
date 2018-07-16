ae22391e1a071b83e28c41558e24119ccee609bf
Mariano Coca <marianoabelcoca@gmail.com>
2018-07-16 19:12:02 -0300

    Add period selection to BotData

--- app/javascript/components/BotData.jsx
+++ app/javascript/components/BotData.jsx
@@ -1,14 +1,23 @@
 import React, { Component } from 'react'
-import { Button, Divider } from 'react-md'
+import { Button, Divider, SelectField } from 'react-md'
 
 import { EmptyContent } from '../ui/EmptyContent'
 import { Headline } from '../ui/Headline'
 import { hasPermission } from '../utils'
 import ContentDenied from './ContentDenied'
 
+const DEFAULT_PERIOD = "none"
+
 export class BotData extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      period: DEFAULT_PERIOD
+    }
+  }
   render() {
     const { bot } = this.props
+    const { period } = this.state
 
     if (!hasPermission(bot, 'manages_results')) {
       return <ContentDenied />
@@ -26,8 +35,22 @@ export class BotData extends Component {
 
     return (
       <EmptyContent icon='file_download'>
-        <Button flat secondary href={`/api/v1/bots/${bot.id}/data.csv`}>Download CSV</Button>
-        <p>or download as <a href={`/api/v1/bots/${bot.id}/data.json`} target="_blank" className="hrefLink">json</a></p>
+        <SelectField
+          id="period"
+          menuItems={[
+            {label: "Do not include previous results", value: "none"},
+            {label: "Include results from today", value: "today"},
+            {label: "Include results from this week", value: "this_week"},
+            {label: "Include results from this month", value: "this_month"},
+            {label: "Include all previous results", value: "all"}
+          ]}
+          stripActiveItem={false}
+          position={SelectField.Positions.BELOW}
+          sameWidth={false}
+          value={period}
+          onChange={value => this.setState({period: value})}/>
+        <Button flat secondary href={`/api/v1/bots/${bot.id}/data.csv?period=${period}`}>Download CSV</Button>
+        <p>or download as <a href={`/api/v1/bots/${bot.id}/data.json?period=${period}`} target="_blank" className="hrefLink">json</a></p>
       </EmptyContent>
     )
   }
