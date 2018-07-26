ed667aa1c1c57b1963a4ebb8cf74d1ff059a0e79
aeatencio <aeatencio@gmail.com>
2018-07-26 11:46:00 -0300

    Refactor BotBehaviour component
    Remove extrinsic code added in 9d718e2.
    Included by error. Unnecesary to solve the issue.
    
    For #228

--- app/javascript/components/BotBehaviour.jsx
+++ app/javascript/components/BotBehaviour.jsx
@@ -9,7 +9,6 @@ import BotSkill from './BotSkill'
 import { hasPermission } from '../utils'
 import ContentDenied from './ContentDenied'
 import { bindActionCreators } from 'redux'
-import { withRouter } from 'react-router'
 import { connect } from 'react-redux'
 
 export class BotBehaviourComponent extends Component {
@@ -37,21 +36,8 @@ export class BotBehaviourComponent extends Component {
   }
 }
 
-const mapStateToProps = (state, {match}) => {
-  let bot = null
-
-  if (state.bots.items) {
-    bot = state.bots.items[match.params.id] || null
-  }
-
-  return {
-    botsLoaded: state.bots.items != null,
-    bot: bot
-  }
-}
-
 const mapDispatchToProps = (dispatch) => ({
   botActions: bindActionCreators(botActions, dispatch)
 })
 
-export const BotBehaviour = withRouter(connect(mapStateToProps, mapDispatchToProps)(BotBehaviourComponent))
+export const BotBehaviour = connect(null, mapDispatchToProps)(BotBehaviourComponent)
