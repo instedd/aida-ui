0fb4f9add775edd573f4dd72d5063917bd6f5f6d
aeatencio <aeatencio@gmail.com>
2018-07-30 13:17:40 -0300

    Show errors in HumanOverride component
    Resolves #181

--- app/javascript/components/HumanOverride.jsx
+++ app/javascript/components/HumanOverride.jsx
@@ -8,7 +8,7 @@ import RelevanceField from './RelevanceField'
 
 export default class HumanOverride extends Component {
   render() {
-    const { skill, actions } = this.props
+    const { skill, actions, errors } = this.props
     const { name, config } = skill
 
     const updateConfig = (key) => {
@@ -34,15 +34,20 @@ export default class HumanOverride extends Component {
         <RelevanceField value={config.relevant} onChange={updateConfig('relevant')} />
 
         <Field id="kr-explanation" label="Skill explanation"
-               value={config.explanation} onChange={updateConfig('explanation')} />
+               value={config.explanation} onChange={updateConfig('explanation')}
+               error={errors.filter(e => e.path[1] == "explanation/en")} />
         <Field id="kr-clarification" label="Skill clarification"
-               value={config.clarification} onChange={updateConfig('clarification')} />
+               value={config.clarification} onChange={updateConfig('clarification')}
+               error={errors.filter(e => e.path[1] == "clarification/en")} />
         <Field id="kr-keywords" label="Valid keywords (comma separated)"
-               value={config.keywords} onChange={updateConfig('keywords')} />
+               value={config.keywords} onChange={updateConfig('keywords')}
+               error={errors.filter(e => e.path[1] == "keywords/en")} />
         <Field id="kr-in-hours-response" label="Within schedule message"
-               value={config.in_hours_response} onChange={updateConfig('in_hours_response')} />
+               value={config.in_hours_response} onChange={updateConfig('in_hours_response')}
+               error={errors.filter(e => e.path[1] == "in_hours_response/en")} />
         <Field id="kr-off-hours-response" label="Outside schedule message"
-               value={config.off_hours_response} onChange={updateConfig('off_hours_response')} />
+               value={config.off_hours_response} onChange={updateConfig('off_hours_response')}
+               error={errors.filter(e => e.path[1] == "off_hours_response/en")} />
       </div>
     )
   }
