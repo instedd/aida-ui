9b54481b76fe210b1ea76b06fd9732b893a521f7
Mariano Coca <marianoabelcoca@gmail.com>
2018-08-02 15:40:19 -0300

    [ci skip] WIP for instedd/aida#220 toggle hours

--- app/javascript/ui/WeeklySchedule.jsx
+++ app/javascript/ui/WeeklySchedule.jsx
@@ -3,12 +3,31 @@ import { TextField } from 'react-md';
 import classNames from 'classnames/bind'
 
 export class WeeklySchedule extends Component {
+  toggle(hourMatrix, dayIndex, hourIndex) {
+    const previousDays = hourMatrix.slice(0, dayIndex)
+    const day = hourMatrix[dayIndex]
+    const nextDays = hourMatrix.slice(dayIndex + 1)
+
+    const previousHours = day.slice(0, hourIndex)
+    const hour = day[hourIndex]
+    const nextHours = day.slice(hourIndex + 1)
+
+    return ([
+      ...previousDays,
+      [
+        ...previousHours,
+        !hour,
+        ...nextHours,
+      ],
+      ...nextDays
+    ])
+  }
+
   render() {
-    const {label, hours, onChange, id, helpText, leftIcon, placeholder, style, className, resize, defaultValue, readOnly, error} = this.props
+    const {hours, onChange} = this.props
 
     const onClick = (dayIndex, hourIndex) => {
-      console.log(dayIndex, hourIndex)
-      // onChange(hours)
+      onChange(this.toggle(hours, dayIndex, hourIndex))
     }
 
     return <div>
