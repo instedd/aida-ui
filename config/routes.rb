152233b1b7e18c0d833d325c00a2127e996528be
Andrés Atencio <aatencio@manas.tech>
2018-09-11 09:59:07 -0300

    Make message route relative to notifications_url
    For instedd/aida#261
    
    Co-authored-by: Matias Garcia Isaia <mgarcia@manas.tech>

--- config/routes.rb
+++ config/routes.rb
@@ -89,7 +89,7 @@ Rails.application.routes.draw do
   get "/invitation/:token", to: 'welcome#index', as: 'invitation'
 
   post '/notifications/:notifications_secret', to: 'api/notifications#create', as: 'notifications_create'
-  post '/notifications/messages/:notifications_secret/:uuid', to: 'api/notifications#create_message', as: 'notifications_create_message'
+  post '/notifications/:notifications_secret/messages/:uuid', to: 'api/notifications#create_message', as: 'notifications_create_message'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
